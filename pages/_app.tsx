d0ca7e0f4097568ec51b2e5e2af067e436e319c1
Brian Wagner <brian.wagner35@gmail.com>
2022-11-04 20:13:08 -0700

    Meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,13 @@ import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
 
+import Meta from '../components/Meta'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Meta />
+      <Component {...pageProps} />
+    </>
+  )
 }
