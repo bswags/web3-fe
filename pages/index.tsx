efb553ada7288da79dd560320b73884a19dc5f83
Brian Wagner <brian.wagner35@gmail.com>
2022-11-04 20:42:37 -0700

    ConnectKit integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
+import { ConnectKitButton } from 'connectkit'
+import { useEffect } from 'react'
+import { useAccount } from 'wagmi'
+
 export default function Home() {
+  const { address } = useAccount()
+
+  useEffect(() => {
+    console.info('account is now', address)
+  }, [address])
+
   return (
-    <div className="w-full h-full min-h-screen flex">
-      <h1 className="m-5">gm</h1>
+    <div className="w-full h-full min-h-screen flex flex-col items-center justify-center">
+      <div className="">
+        <ConnectKitButton theme="rounded" />
+      </div>
     </div>
   )
 }
